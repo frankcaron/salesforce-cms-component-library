6c00a847e7aefde8da0168cb8b76aba57d92cf57
Frank Caron <frank@frankcaron.com>
2020-10-27 12:31:45 -0400

    Added apex imports
    Also included reference code from original Aura controllers to rewire appropriately

--- force-app/main/default/lwc/lWCCMSContentByRecord/lWCCMSContentByRecord.js
+++ force-app/main/default/lwc/lWCCMSContentByRecord/lWCCMSContentByRecord.js
@@ -1,3 +1,41 @@
 import { LightningElement } from 'lwc';
+import getCMSContentForRecordTopics from '@salesforce/apex/cmsutils.ManagedContentController';
 
-export default class lwcCMSContentByRecord extends LightningElement {}
+export default class lwcCMSContentByRecord extends LightningElement {
+
+}
+
+/* 
+({
+	getContentForRecordTopics : function(component, event, helper) {
+		var action = component.get("c.getCMSContentForRecordTopics");
+        action.setParams({recordId : component.get("v.recordId"), managedContentType : component.get("v.managedContentType")});
+        
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var myContent = response.getReturnValue();
+                component.set("v.CMSContent", myContent);
+                
+                // set values to help debug
+                console.log("CMS Content: " + myContent);
+                //alert(JSON.stringify(myContent));
+				component.set("v.stringCMSContent", JSON.stringify(response.getReturnValue()));
+            } else if (state === "INCOMPLETE") {
+                console.log("State incomplete.");
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+        
+        $A.enqueueAction(action);                 
+	}
+})
+*/
--- force-app/main/default/lwc/lWCCMSContentByTypeAndTopic/lWCCMSContentByTypeAndTopic.js
+++ force-app/main/default/lwc/lWCCMSContentByTypeAndTopic/lWCCMSContentByTypeAndTopic.js
@@ -1,3 +1,40 @@
 import { LightningElement } from 'lwc';
+import getCMSContentForTopic from '@salesforce/apex/cmsutils.ManagedContentController';
 
-export default class lwcCMSContentByTypeAndTopic extends LightningElement {}
+export default class lwcCMSContentByTypeAndTopic extends LightningElement {
+
+}
+
+/* ({
+	getContentForTopic : function(component, event, helper) {
+        var action = component.get("c.getCMSContentForTopic");
+        action.setParams({topicId : component.get("v.topicId"), managedContentType : component.get("v.managedContentType")});
+        
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var myContent = response.getReturnValue();
+                component.set("v.CMSContent", myContent);
+                
+                // set values to help debug
+                console.log("CMS Content: " + myContent);
+                //alert(JSON.stringify(myContent));
+				component.set("v.stringCMSContent", JSON.stringify(response.getReturnValue()));
+            } else if (state === "INCOMPLETE") {
+                console.log("State incomplete.");
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+        
+        $A.enqueueAction(action);
+	}
+})
+*/
